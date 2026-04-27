dc63e93fc60370e30c43d2becfb6400fafe5f736
Gilberto Bertin <me@jibi.io>
2026-04-27 10:26:29 +0200

    home: vim: don't enforce .md's linewidth for code blocks

--- modules/home/vim/lua/config/autocmds.lua
+++ modules/home/vim/lua/config/autocmds.lua
@@ -23,9 +23,36 @@ local md_autowrap = vim.api.nvim_create_augroup("md_autowrap", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   group = md_autowrap,
   pattern = "markdown",
-  callback = function()
+  callback = function(event)
     vim.opt_local.textwidth = 80
     vim.opt_local.formatoptions:append("t")
     vim.opt_local.formatoptions:append("a")
+
+    vim.api.nvim_create_autocmd({ "CursorMoved", "InsertEnter" }, {
+      group = md_autowrap,
+      buffer = event.buf,
+      callback = function()
+        local ok, node = pcall(vim.treesitter.get_node)
+        local in_code = false
+        while ok and node do
+          local t = node:type()
+          if t == "fenced_code_block" or t == "code_fence_content" or t == "indented_code_block" then
+            in_code = true
+            break
+          end
+          node = node:parent()
+        end
+        local fo = vim.bo.formatoptions
+        local has_t = fo:find("t") ~= nil
+        local has_a = fo:find("a") ~= nil
+        if in_code then
+          if has_t then vim.opt_local.formatoptions:remove("t") end
+          if has_a then vim.opt_local.formatoptions:remove("a") end
+        else
+          if not has_t then vim.opt_local.formatoptions:append("t") end
+          if not has_a then vim.opt_local.formatoptions:append("a") end
+        end
+      end,
+    })
   end,
 })
