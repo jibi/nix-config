338cdc0463913b1e9a488a56c930ece11357b6f3
Gilberto Bertin <me@jibi.io>
2026-05-22 19:51:15 +0200

    home: ssh: remove old config

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -6,11 +6,5 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
-      debian = {
-        hostname = "192.168.122.182";
-        identityFile = "~/.ssh/id_debian";
-      };
-    };
   };
 }
